d867c7f93369eb3d6de2e63fd2faffbcf5c93e6a
tinahumu <tinahumu@gmail.com>
2022-12-12 17:06:57 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints the number in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char al;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (al = 'a'; al <= 'f'; al++)
+		putchar(al);
+
+	putchar('\n');
+
+	return (0);
+}
