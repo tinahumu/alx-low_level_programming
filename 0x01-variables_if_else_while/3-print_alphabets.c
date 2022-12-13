fded2ca4aac8cb7b042feee1bd359ff9698da5f4
tinahumu <tinahumu@gmail.com>
2022-12-12 16:10:30 -0800

    print upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main -print alphabets in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		putchar(al);
+
+	for (al = 'A'; al <= 'Z'; al++)
+		putchar(al);
+
+	putchar('\n');
+
+	return (0);
+}
