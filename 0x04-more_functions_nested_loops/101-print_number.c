d725b04962209e1f3aaf4219f725710f645b3302
tinahumu <tinahumu@gmail.com>
2022-12-16 18:54:57 -0800

    12. Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,9 +9,8 @@ void print_integer(int m);
 void print_number(int n)
 {
 	if (n == 0)
-	{
+	
 		_putchar('0');
-	}
 	else if (n < 0)
 	{
 		_putchar('-');
@@ -26,6 +25,7 @@ void print_number(int n)
  * @m: an input unsigned integer
  * Return: Nothing
  */
+
 void print_integer(int m)
 {
 	int i = 1000000000;
