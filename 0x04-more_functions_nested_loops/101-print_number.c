613db55168543b112aa4d62e2aa9ff0f3bd918a7
tinahumu <tinahumu@gmail.com>
2022-12-16 18:48:46 -0800

    12. Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,10 @@ void print_number(int n)
 		_putchar('0');
 	}
 	else if (n < 0)
-		{
-			_putchar('-');
-			print_integer(n * -1);
-		}
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
 	else
 		print_integer(n);
 }
